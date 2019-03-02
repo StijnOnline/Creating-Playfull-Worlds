bdc5a653360b3e372397aa475383c4746a05c0b1
StijnOnline <stijnvandeijzen@gmail.com>
2019-03-02 17:26:34 +0100

    Added Laser preview button, Fixed Interactable Bug

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -19,7 +19,10 @@ public class Weapon : MonoBehaviour {
 
     LineRenderer lr;
     float counter, lineDrawSpeed;
-    Interactable hit_interactable;
+
+    private IEnumerator coroutine;
+
+    public GameObject preview;
 
 
     void Awake(){
@@ -31,6 +34,8 @@ public class Weapon : MonoBehaviour {
         layerMask = 1 << LayerMask.NameToLayer("RayCastIgnore");
         layerMask = ~layerMask;
 
+        preview = Instantiate(preview);
+        preview.SetActive(false);
     }
 
     void Update() {
@@ -54,6 +59,14 @@ public class Weapon : MonoBehaviour {
             Shoot();
         }
 
+        
+        if (Input.GetMouseButton(1)) {
+            preview.SetActive(true);
+            Preview();
+        } else {
+            preview.SetActive(false);
+        }
+        
 
     }
 
@@ -139,12 +152,14 @@ public class Weapon : MonoBehaviour {
 
             ////check for Interactables
             if (hit.collider.tag == "Interactable") {
-                hit_interactable = hit.collider.gameObject.GetComponent<Interactable>();
+                Interactable hit_interactable = hit.collider.gameObject.GetComponent<Interactable>();
                 if (hit_interactable.color == color) {
                     Laser lsr_script = lsr.GetComponent<Laser>();
-                    float distance = Vector3.Distance(lsr_script.positions[0], lsr_script.positions[1]) + Vector3.Distance(lsr_script.positions[1], lsr_script.positions[2]);
-                    Invoke("Interact", distance / GameManager.laserspeed);
-
+                    float distance = Vector3.Distance(lsr_script.positions[0], lsr_script.positions[1]) ;
+                    distance += Vector3.Distance(lsr_script.positions[1], lsr_script.positions[2]);
+                    distance -= lsr_script.length * 2;
+                    coroutine = Interact(distance / GameManager.laserspeed,hit_interactable);
+                    StartCoroutine(coroutine);
                 }
 
             }
@@ -165,10 +180,78 @@ public class Weapon : MonoBehaviour {
         }
     }
 
-    void Interact()
+    void Preview() {
+        LineRenderer lr = preview.GetComponent<LineRenderer>();
+        lr.SetPosition(0, muzzle_pos.transform.position);
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, Mathf.Infinity, layerMask)) {
+            
+            lr.SetPosition(1, hit.point);
+            Debug.Log(hit.point);
+            //check for barriers before mirror
+            while (hit.collider.tag == "Barrier") {
+                GameObject ob = hit.collider.gameObject;
+                Interactable hit_interactable = hit.collider.gameObject.GetComponent<Interactable>();
+                if ((hit_interactable.type == 4 && hit_interactable.color == color) || hit_interactable.type == 3) {
+                    ob.layer = LayerMask.NameToLayer("RayCastIgnore");
+
+                    Vector3 incomingVec = fpscam.transform.forward;
+                    if (Physics.Raycast(hit.point, incomingVec, out hit, Mathf.Infinity, layerMask)) {
+                        lr.SetPosition(1, hit.point);
+                    }
+                    ob.layer = 0;
+                } else {
+                    break;
+                }
+            }
+
+            //check for mirror
+            if (hit.collider.tag == "Mirror") {
+                Vector3 incomingVec = fpscam.transform.forward;
+                Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
+
+                if (Physics.Raycast(hit.point, reflectVec, out hit)) {
+
+                    lr.positionCount = 3;
+                    lr.SetPosition(2, hit.point);
+
+                    //check for barriers after mirror
+                    while (hit.collider.tag == "Barrier") {
+                        GameObject ob = hit.collider.gameObject;
+                        Interactable hit_interactable = hit.collider.gameObject.GetComponent<Interactable>();
+                        if ((hit_interactable.type == 4 && hit_interactable.color == color) || hit_interactable.type == 3) {
+                            ob.layer = LayerMask.NameToLayer("RayCastIgnore");
+
+                            if (Physics.Raycast(hit.point, reflectVec, out hit, Mathf.Infinity, layerMask)) {
+                                lr.SetPosition(2, hit.point);
+                            }
+
+                            ob.layer = 0;
+                        } else {
+                            break;
+                        }
+                    }
+
+
+
+                }
+            }
+
+            //set colors
+            lr.startColor = GameManager.colors[color];
+            lr.endColor = GameManager.colors[color];
+
+        } else {
+            lr.SetPosition(1, fpscam.transform.position + fpscam.transform.forward * 500);
+        }
+    }
+
+    private IEnumerator Interact(float waitTime,Interactable hit_interactable)
     {
-        if (hit_interactable.type == 0) { GameManager.color_states[color] = true; }
-        if (hit_interactable.type == 1) { GameManager.color_states[color] = !GameManager.color_states[color]; }
+        yield return new WaitForSeconds(waitTime);
+        if (hit_interactable.type == 0) { GameManager.color_states[hit_interactable.color] = true; }
+        if (hit_interactable.type == 1) { GameManager.color_states[hit_interactable.color] = !GameManager.color_states[hit_interactable.color]; }
     }
 
 }
